c7f9dc2cc6b51f8b58c850c5ba53fbc83f1ccf64
Andrea Schweer <schweer@waikato.ac.nz>
2015-08-29 14:46:49 +1200

    Multi-format export

--- src/main/java/nz/ac/waikato/its/dspace/exportcitation/ItemCitationExporter.java
+++ src/main/java/nz/ac/waikato/its/dspace/exportcitation/ItemCitationExporter.java
@@ -4,38 +4,59 @@ import org.apache.avalon.excalibur.pool.Recyclable;
 import org.apache.avalon.framework.parameters.ParameterException;
 import org.apache.avalon.framework.parameters.Parameters;
 import org.apache.cocoon.ProcessingException;
+import org.apache.cocoon.environment.ObjectModelHelper;
 import org.apache.cocoon.environment.Request;
 import org.apache.cocoon.environment.Response;
+import org.apache.cocoon.environment.SourceResolver;
+import org.apache.cocoon.reading.AbstractReader;
 import org.dspace.app.xmlui.utils.ContextUtil;
 import org.dspace.authorize.AuthorizeException;
 import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
 import org.dspace.content.crosswalk.CrosswalkException;
+import org.dspace.content.crosswalk.StreamDisseminationCrosswalk;
 import org.dspace.core.Context;
-import org.apache.cocoon.environment.ObjectModelHelper;
-import org.apache.cocoon.environment.SourceResolver;
-import org.apache.cocoon.reading.AbstractReader;
 import org.dspace.handle.HandleManager;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author Andrea Schweer schweer@waikato.ac.nz for Waikato University ITS
  */
 public class ItemCitationExporter extends AbstractReader implements Recyclable {
+	public static final String FORMAT_ENDNOTE = "endnote";
+	public static final String FORMAT_WORD_CITATIONONLY = "word-citation";
+	public static final String FORMAT_WORD_CITATIONABSTRACT = "word-citationabstract";
+
 	private static final String SEPARATOR = "  - ";
 
 	private Request request;
 	private Response response;
 
-	EndnoteExportCrosswalk crosswalk = new EndnoteExportCrosswalk();
+	private static Map<String, CitationDisseminationCrosswalk> formatToCrosswalk = new HashMap<>();
+
+	static {
+		if (formatToCrosswalk.isEmpty()) {
+			formatToCrosswalk.put(FORMAT_ENDNOTE, new EndnoteExportCrosswalk());
+			formatToCrosswalk.put(FORMAT_WORD_CITATIONONLY, new WordCitationExportCrosswalk(false));
+			formatToCrosswalk.put(FORMAT_WORD_CITATIONABSTRACT, new WordCitationExportCrosswalk(true));
+		}
+	}
+	private static Map<String, String> mimetypeToExtension = new HashMap<>();
+
+	static {
+		mimetypeToExtension.put("application/x-research-info-systems", ".ris");
+		mimetypeToExtension.put("application/vnd.openxmlformats-officedocument.wordprocessingml.document", ".docx");
+	}
 
 	private String filename;
 	private Context context;
 	private Item item;
+	private CitationDisseminationCrosswalk crosswalk;
 
 	@Override
 	public void setup(SourceResolver resolver, Map objectModel, String src, Parameters par) throws ProcessingException, SAXException, IOException {
@@ -50,7 +71,14 @@ public class ItemCitationExporter extends AbstractReader implements Recyclable {
 			DSpaceObject dso = HandleManager.resolveToObject(context, handle);
 			item = (Item) dso;
 
-			filename = "cite-" + handle.replaceAll("/", "-") + ".ris";
+			String format = par.getParameter("format", "endnote");
+			if (formatToCrosswalk.containsKey(format)) {
+				crosswalk = formatToCrosswalk.get(format);
+			} else {
+				throw new ProcessingException("Unknown citation export format " + format + ", cannot proceed");
+			}
+
+			filename = "cite-" + handle.replaceAll("/", "-");
 		} catch (SQLException | ParameterException e) {
 			throw new ProcessingException("Unable to export citation.", e);
 		}
@@ -59,14 +87,11 @@ public class ItemCitationExporter extends AbstractReader implements Recyclable {
 	@Override
 	public void generate() throws IOException, SAXException, ProcessingException {
 		response.setContentType(crosswalk.getMIMEType());
-		response.setHeader("Content-Disposition", "attachment; filename=" + filename);
-
-		crosswalk.writeHeader(out);
+		response.setHeader("Content-Disposition", "attachment; filename=" + filename + getExtension(crosswalk.getMIMEType()));
 
 		if (crosswalk.canDisseminate(context, item)) {
 			try {
 				crosswalk.disseminate(context, item, out);
-				out.write("\n\n".getBytes());
 			} catch (CrosswalkException | AuthorizeException | SQLException e) {
 				e.printStackTrace();
 			}
@@ -76,6 +101,13 @@ public class ItemCitationExporter extends AbstractReader implements Recyclable {
 		out.close();
 	}
 
+	private String getExtension(String mimeType) {
+		if (mimetypeToExtension.containsKey(mimeType)) {
+			return mimetypeToExtension.get(mimeType);
+		}
+		return "";
+	}
+
 	@Override
 	public void recycle() {
 		this.request = null;
@@ -83,5 +115,6 @@ public class ItemCitationExporter extends AbstractReader implements Recyclable {
 		this.context = null;
 		this.item = null;
 		this.filename = null;
+		this.crosswalk = null;
 	}
 }
